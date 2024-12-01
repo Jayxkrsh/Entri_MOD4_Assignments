e840f98aaae626e37193cec1c28f2564f794c485
Jayxkrsh <jayakrishnanmnair58@gmail.com>
2024-12-01 09:03:08 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,67 @@
+# Exercise_1
+# Name="Jayakrishnan"
+# Student_Number="AJ20CE023"
+# Email_Address="jayakrishnanmnair58@gmail.com"
+# print(Name)
+# print(Student_Number)
+# print(Email_Address)
+
+# Exercise_2
+# print("Jayakrishnan\nAJ20CE023\njayakrishnanmnair58@gmail.com")
+
+# Exercise_3
+# num1 = 14
+# num2 = 7
+# print(f"{num1} + {num2} = {num1 + num2}")
+# print(f"{num1} * {num2} = {num1 * num2}")
+# print(f"{num1} - {num2} = {num1 - num2}")
+# print(f"{num1} / {num2} = {num1 / num2}")
+
+# Exercise_4
+# for number in range(1, 6):
+#  print(number)
+
+# Exercise_5
+# print('"SDK" stands for "Software Development Kit", whereas\n"IDE" stands for "Integrated Development Environment".')
+
+# Exercise 6
+# Practice and check the output
+# print("python is an \"awesome\" language.")
+# print("python\n\t2023")
+# print('I\'m from Entri.\b')
+# print("\65")
+# print("\x65")
+# print("Entri", "2023", sep="\n")
+# print("Entri", "2023", sep="\b")
+# print("Entri", "2023", sep="*", end="\b\b\b\b")
+
+# Exercise_7
+# num=23
+# textnum="57"
+# decimal=98.3
+# print(type(num))
+# print(type(textnum))
+# print(type(decimal))
+#
+# sum_of_variables=num+int(textnum)+decimal
+#
+# print("Sum of variables :",sum_of_variables)
+# print("Type of Sum :",type(sum_of_variables))
+
+# Exercise_8
+# days_in_a_year = 365
+# hours_in_a_day = 24
+# minutes_in_an_hour = 60
+# total_minutes_in_a_year = days_in_a_year * hours_in_a_day * minutes_in_an_hour
+# print("This program calculates the total number of minutes in a year.")
+# print(f"Total minutes in a year: {total_minutes_in_a_year}")
+
+# Exercise_9
+# name = input("Please enter your name: ")
+# print(f"Hi {name}, welcome to Python programming :)")
+
+# Exercise_10
+conversion_rate = 1.25
+amount_in_pounds=float(input("Enter the amount in pounds:"))
+amount_in_dollars=amount_in_pounds*conversion_rate
+print(f"£{amount_in_pounds:.2f} are ${amount_in_dollars:.2f}")
