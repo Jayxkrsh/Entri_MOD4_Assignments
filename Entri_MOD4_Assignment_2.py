b4059c717dbd7845f0938cde219a38617fd24830
Jayxkrsh <jayakrishnanmnair58@gmail.com>
2024-12-07 21:33:20 +0530

    Add files via upload

---
+++ Entri_MOD4_Assignment_2.py
@@ -0,0 +1,44 @@
+# Topic :List
+# Exercise
+# Q1. Create a list of 5 random numbers and print the list.
+# random_numbers=[1,2,3,4,5]
+# print(type(random_numbers),random_numbers)
+
+# Q2. Insert 3 new values to the list and print the updated list.
+# random_numbers.extend([6,7,8])
+# print(random_numbers,type(random_numbers))
+
+# Q3. Try to use a for loop to print each element in the list.
+# print("Elements in the list:")
+# for number in random_numbers:
+#     print(number)
+
+# Topic: Dictionary
+# Exercise
+# Q1. Create a dictionary with keys 'name', 'age', and 'address' and values 'John', 25, and 'New York' respectively.
+# Details={"name":"John","Age":25,"Address":"New York"}
+# print(Details,type(Details))
+
+# Q2. Add a new key-value pair to the dictionary created in Q1 with key 'phone' and value '1234567890'.
+# Details["Phone"]=1234567890
+# print(Details,type(Details))
+
+# Topic: Set
+# Exercise
+# Q1.Create a set with values 1, 2, 3, 4, and 5.
+# Set_1={1,2,3,4,5}
+# print(Set_1,type(Set_1))
+# Q2. Add the value 6 to the set created in Q1.
+# Set_1.add(6)
+# print(Set_1,type(Set_1))
+# Q3. Remove the value 3 from the set created in Q1.
+# Set_1.remove(3)
+# print(Set_1,type(Set_1))
+
+# Topic:Tuple
+# Exercise
+# Q1. Create a tuple with values 1, 2, 3, and 4
+# Tuple_1=1,2,3,4
+# print(Tuple_1,type(Tuple_1))
+# Q2. Print the length of the tuple created in Q1.
+# print(len(Tuple_1))
