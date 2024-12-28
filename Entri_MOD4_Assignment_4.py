b39a7bd8f1b4007f14c314d71ba485bd7d9fc3c2
Jayxkrsh <jayakrishnanmnair58@gmail.com>
2024-12-28 21:23:32 +0530

    Add files via upload

---
+++ Entri_MOD4_Assignment_4.py
@@ -0,0 +1,46 @@
+# Exercise_1
+# Students = ['Jay','Aslam','Suni','Hari']
+# print(f"The length of the list:{len(Students)}")
+'''The len() function is used to return the number of items (length)
+ in an object, such as a string, list, tuple,
+ dictionary, or other iterable objects.'''
+
+# Exercise_2
+# Name1 = input("Enter your name:")
+# print(f'"Hello!,{Name1}"')
+
+# Exercise_3
+# def find_maximum(numbers):
+#     if not numbers:
+#         return None
+#     max_value=numbers[0]
+#     for num in numbers:
+#         if num>max_value:
+#             max_value=num
+#     return max_value
+#
+# numbers = [1,2,3,4,5,6,7,8,9]
+# max_num = find_maximum(numbers)
+# print(f"The max.number is :{max_num}")
+
+# Exercise_4
+# x = 10 # Global variable
+# def my_function():
+#     x = 5 #local variable
+#     print("Local x inside the function:", x)
+# my_function()
+# print("Global x outside the function:", x)
+# '''Here when we call the function (my_function) it reads the x (x=5) inside the block,
+# not the global variable x.'''
+
+# Exercise_5
+# def calculate_area(length, width=5):
+#     area = length * width
+#     return area
+#
+# area1= calculate_area(5,2)
+# print(f"Area with length:5 and width:2 : {area1}")
+#
+# area2 =  calculate_area(3)
+# print(f"Area with length:3 :{area2}")
+
