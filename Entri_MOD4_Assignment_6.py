4717138bd00c7232b3912c653148a56b9c2beb97
Jayxkrsh <jayakrishnanmnair58@gmail.com>
2025-01-17 11:30:08 +0530

    Add files via upload

---
+++ Entri_MOD4_Assignment_6.py
@@ -0,0 +1,37 @@
+# class Course:
+#     def __init__(self,course_code, course_name, credit_hours):
+#         self.course_code = course_code
+#         self.course_name = course_name
+#         self.credit_hours = credit_hours
+#
+#     def __str__(self):
+#         return f"{self.course_code}: {self.course_name} ({self.credit_hours} credit hours)"
+#
+# class CoreCourse(Course):
+#     def __init__(self, course_code, course_name, credit_hours, required_for_major):
+#         super().__init__(course_code, course_name, credit_hours)
+#         self.required_for_major = required_for_major
+#
+#     def __str__(self):
+#         required_status = "Required for Major" if self.required_for_major else "Not Required for Major"
+#         return f"{super().__str__()} - {required_status}"
+#
+# class ElectiveCourse(Course):
+#     def __init__(self, course_code, course_name, credit_hours, elective_type):
+#         super().__init__(course_code, course_name, credit_hours)
+#         self.elective_type = elective_type
+#
+#     def __str__(self):
+#         return f"{super().__str__()} - Elective Type: {self.elective_type}"
+#
+# core_course = CoreCourse("CS101", "Introduction to Computer Science", 3, True)
+# elective_course = ElectiveCourse("ART101", "Introduction to Painting", 3, "Liberal Arts")
+# print(core_course)
+# print(elective_course)
+
+# Qus_2
+# from employee import Employee
+#
+# EMP = Employee("Jay",200000)
+# print("Employee Name:",EMP.get_name())
+# print("Employee Salary:",EMP.get_salary())
